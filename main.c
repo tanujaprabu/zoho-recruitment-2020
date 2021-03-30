3308a5d88bd1984bb857123767c01aaf55fd23e5
tanujaprabu <38122935+tanujaprabu@users.noreply.github.com>
2021-03-30 10:24:19 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+
+void main()
+{
+    char N1=Ram,N2=Bhavana,N3=Agilan,N4=Yamini,N5=Megala;
+    char F1=Ramesh,F2=Ram,F3=Elango;
+    char G1=Male,G2=Female;
+    char M1=Revathy,M2=Dharshini,M3=Bavana;
+    scanf("N1=%c,G1=%c,F1=%c,M1=%c" , &N1, &G1, &F1, &M1);
+    scanf("N2=%c,G2=%c,F1=%c,M1=%c" , &N1, &G2, &F1, &M1);
+    scanf("N3=%c,G1=%c,F2=%c,M2=%c" , &N1, &G1, &F1, &M1);
+    scanf("N4=%c,G2=%c,F3=%c,M3=%c" , &N1, &G2, &F1, &M1);
+    scanf("N5=%c,G2=%c,F3=%c,M3=%c" ,&N1, &G2, &F1, &M1);
+
+
+if(Person Nane = N1)
+{
+  if(G1==Male&F1!=F2&F1!=F3&M1!=M2&M2!=M3)
+  {
+      printf(N4,N5);
+  }
+  else(Person Name = N2 )
+  {
+     if(G2==Female&F1!=F2&F1!=F3&M1!=M2&M2!=M3)
+        {
+            printf(N3);
+        }
+  }
+
+
+  }
+}
+return 0;
+}
+
+}
+
